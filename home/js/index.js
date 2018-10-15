7735fa38b158b46ef474e2231a49073f076083f4
庄宇韬 <455171468@qq.com>
2018-10-15 11:18:04 +0800

    pages / home

---
+++ home/js/index.js
@@ -0,0 +1,41 @@
+~function(){
+
+    var $home_btn1 = $("#btn1")
+    var $home_btn2 = $("#btn2")
+    var $home_btn3 = $("#btn3")
+    var $home_btn4 = $("#btn4")
+
+   var router = new Router()
+
+    //首页按钮
+    //时光印象
+    $home_btn1.on("touchend",function(){
+
+        router.to( 1 )
+
+    })
+
+    //忆江南
+    $home_btn2.on("touchend",function(){
+
+        router.to( 2 )
+
+    })
+
+
+    //晚堂香
+    $home_btn3.on("touchend",function(){
+
+        router.to( 3 )
+
+    })
+
+
+    //中国禅
+    $home_btn4.on("touchend",function(){
+
+        router.to( 4 )
+
+    })
+
+}()
